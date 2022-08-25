4f31d8ef5a19feb4e63ba45f64f06dfc5989acd7
LENOVO <LENOVO@PC-f7ad11>
2022-08-25 13:47:42 +0530

    wrapper

--- src/pak/Run.java
+++ src/pak/Run.java
@@ -1,10 +1,51 @@
 package pak;
 
-public class Run {
+class A
+{
+	
+	static void test(Object obj)
+	{
+		String classname=obj.getClass().toString();
+		
+		
+		switch(classname)
+		{
+		case "java.lang.Integer":
+			 int i= ((Integer)obj).intValue(); 
+			 System.out.println(i);
+			 break;
+		case "java.lang.Double":	 
+			 double d=((Double)obj).doubleValue();
+			 System.out.println(d);
+			 break;
+		case "java.lang.String":
+			String str=((String)obj).toString();
+			System.out.println(str);
+			break;
+	   default:
+		   System.out.println("Invalid input");
+		
+		
+		}
+		
+	}
+	
+}
+public class Run 
+
+{
 
 	public static void main(String[] args)
-	{
-	  System.out.println("hi");
+	{	
+		Integer i1=new Integer(10); //boxing		
+	    A.test(i1);
+	    
+	    Double d1=new Double(10.02);
+	    A.test(d1);
+	    
+	    String str1=new String("Jimmy");
+	    A.test(str1);
+		
 	
 	}
 
